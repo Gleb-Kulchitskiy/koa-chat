c12a0ba25308e009675eb3070aae84d1c4231e8d
Gleb Kulchitskiy <glebkulchitskiy@gmail.com>
2017-12-04 01:31:17 +0200

    no message

--- app.js
+++ app.js
@@ -48,55 +48,51 @@ async function saveToDb(ctx, next) {
   await Message.create({email: ctx.request.body.email, message: ctx.request.body.message})
     .then(() => console.log('saved'));
   
-  async function saveToDb(ctx, next) {
-    await Message.create({email: ctx.request.body.email, message: ctx.request.body.message})
-      .then(() => console.log('saved'));
-    
-    await next();
-  }
+  await next();
+}
+
+router.post('/publish', saveToDb, async (ctx, next) => {
   
-  router.post('/publish', saveToDb, async (ctx, next) => {
-    
-    const message = ctx.request.body.message;
-    
-    if (!message) {
-      ctx.throw(400);
-    }
-    users.forEach(function (resolve) {
-      resolve(String(message));
-    });
-    
-    users = [];
-    
-    ctx.body = 'ok';
-    
+  const message = ctx.request.body.message;
+  
+  if (!message) {
+    ctx.throw(400);
+  }
+  users.forEach(function (resolve) {
+    resolve(String(message));
   });
   
-  async function loadMessageById(ctx, next) {
-    if (!mongoose.Types.ObjectId.isValid(ctx.params.messageById)) {
-      ctx.throw(400);
-    }
-    ctx.messageById = await Message.findById(ctx.params.messageById);
-    if (!ctx.messageById) {
-      ctx.throw(400);
-    }
-    await next();
+  users = [];
+  
+  ctx.body = 'ok';
+  
+});
+
+async function loadMessageById(ctx, next) {
+  if (!mongoose.Types.ObjectId.isValid(ctx.params.messageById)) {
+    ctx.throw(400);
   }
-  
-  async function loadPaginationMessages(ctx, next) {
-    const number = ctx.params.number * 10;
-    await Message.paginate({}, {offset: number, limit: 10})
-      .then((result) => ctx.paginationMessages = result);
-    await next();
+  ctx.messageById = await Message.findById(ctx.params.messageById);
+  if (!ctx.messageById) {
+    ctx.throw(400);
   }
-  
-  router
-    .get('/messages/single/:messageById', loadMessageById, function (ctx) {
-      ctx.body = ctx.messageById.toObject();
-    })
-    .get('/messages/list/:number', loadPaginationMessages, function (ctx) {
-      ctx.body = ctx.paginationMessages;
-    });
-  
-  app.use(router.routes());
-  app.listen(3000);
+  await next();
+}
+
+async function loadPaginationMessages(ctx, next) {
+  const number = ctx.params.number * 10;
+  await Message.paginate({}, {offset: number, limit: 10})
+    .then((result) => ctx.paginationMessages = result);
+  await next();
+}
+
+router
+  .get('/messages/single/:messageById', loadMessageById, function (ctx) {
+    ctx.body = ctx.messageById.toObject();
+  })
+  .get('/messages/list/:number', loadPaginationMessages, function (ctx) {
+    ctx.body = ctx.paginationMessages;
+  });
+
+app.use(router.routes());
+app.listen(3000);
